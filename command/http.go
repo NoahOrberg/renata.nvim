15cd6db97c76bcb065f21535fc3ea71bd1af3bca
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-08 22:06:03 +0900

    wip

--- command/http.go
+++ command/http.go
@@ -1,17 +1,51 @@
 package command
 
 import (
-	"strconv"
+	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/neovim/go-client/nvim"
 )
 
 func (r *Renata) RenataHttp(v *nvim.Nvim, args []string) error {
-	v.Command("echom " + strconv.Itoa(len(args)))
-	v.Command("echom " + args[0])
+	if len(args) < 2 {
+		return errors.New("usage: :Http <METHOD> <URL> ( <HEADER>:<VALUE>... )")
+	}
+	method := args[0]
+	if method != "GET" && method != "POST" && method != "PUT" && method != "PATCH" && method != "DELETE" {
+		return fmt.Errorf("unknown this command %s", method)
+	}
+
+	url := args[1]
+
 	// バッファを読む
-	// それが妥当なJSONか確認する
-	// 引数一番目にメソッド、二番目にURLを受け取る
-	// 第三引数以降でヘッダなどを追加できるようにする
+	buf, err := v.CurrentBuffer()
+	if err != nil {
+		return err
+	}
+
+	lines, err := v.BufferLines(buf, 0, -1, true)
+	size := 0
+	for _, l := range lines {
+		size += len(l)
+	}
+
+	line := make([]byte, 0, size+len(lines))
+	for _, l := range lines {
+		l = []byte(strings.TrimSpace(string(l)))
+		line = append(line, l...)
+	}
+
+	payload := string(line)
+
+	switch method {
+	case "GET":
+		getRequest(url, payload)
+	}
+	return nil
+}
+
+func postRequest(url, payload string, header map[string]string) error {
 	return nil
 }
